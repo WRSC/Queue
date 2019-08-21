306e1902dea20bd62c79e26cf4ca1fdf5e545c43
Yu Cao <tsaoyu@tsaoyu.com>
2019-08-21 13:31:04 -0400

    Save example

--- RaceQueue.py
+++ RaceQueue.py
@@ -60,24 +60,5 @@ class RaceQueue():
              
 if __name__ == "__main__":
     rq = RaceQueue()
-    rq.register_teams(['Shanghai','Zhejiang','Soton'])
-
-    rq.register_team('Plymouth')
-    time.sleep(5)
-    rq.register_attempt('Plymouth')
-    time.sleep(5)
-    rq.register_attempt('Shanghai')
-    time.sleep(5)
-    rq.register_attempt('Soton')
-    time.sleep(5)
-    rq.register_attempt('Zhejiang')
-    time.sleep(5)
-    print(rq.next_ready_teams(number=2))
-    rq.finish_attempt('Shanghai')
-    time.sleep(5)
-    print(rq.next_ready_teams())
-    time.sleep(5)
-    rq.register_attempt('Soton') # Unsuccessful attempt
-    print(rq.next_ready_teams())
-    print(rq.race_history)
+    print('Please start to register the attempt.')
 
